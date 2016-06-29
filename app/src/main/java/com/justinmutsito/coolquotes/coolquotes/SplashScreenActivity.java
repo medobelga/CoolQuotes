f65a1097ab9e4420178bd278cd2a5de845640b9e
Justin Mutsito <justinmutsito@gmail.com>
2016-06-29 13:01:17 +0100

    Added quotes ,implemented splash screen and welcome activity.

--- app/src/main/java/com/justinmutsito/coolquotes/coolquotes/SplashScreenActivity.java
+++ app/src/main/java/com/justinmutsito/coolquotes/coolquotes/SplashScreenActivity.java
@@ -1,5 +1,6 @@
 package com.justinmutsito.coolquotes.coolquotes;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
@@ -10,5 +11,23 @@ public class SplashScreenActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash_screen);
         getSupportActionBar().hide();
+
+            Thread timer = new Thread(){
+                public void run(){
+                    try {
+                        sleep(2000);
+                    } catch (InterruptedException e) {
+                        //IntentionalLy left blank
+                    }
+                    finally {
+                        Intent intent = new Intent(SplashScreenActivity.this,WelcomeActivity.class);
+                        intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+                        intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK);
+                        startActivity(intent);
+                    }
+                }
+            };
+        timer.start();
+
     }
 }
