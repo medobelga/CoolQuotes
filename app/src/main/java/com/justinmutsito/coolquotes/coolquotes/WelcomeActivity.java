63dec0112a28de6d9653626fa286c83719e8315e
Justin Mutsito <justinmutsito@gmail.com>
2016-06-30 23:06:24 +0100

    Added views to welcome activity layout,.

--- app/src/main/java/com/justinmutsito/coolquotes/coolquotes/WelcomeActivity.java
+++ app/src/main/java/com/justinmutsito/coolquotes/coolquotes/WelcomeActivity.java
@@ -2,13 +2,24 @@ package com.justinmutsito.coolquotes.coolquotes;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import butterknife.Bind;
+import butterknife.ButterKnife;
 
 public class WelcomeActivity extends AppCompatActivity {
 
+    @Bind(R.id.faceImageView) ImageView mFace;
+    @Bind(R.id.quoteTextView) TextView mQuote;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_welcome);
         getSupportActionBar().hide();
+        ButterKnife.bind(this);
+
+
+
     }
 }
