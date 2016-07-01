2e89175d78f96187e7ed766acb33654a07dba38e
Justin Mutsito <justinmutsito@gmail.com>
2016-07-01 15:47:28 +0100

    Implemented random quotes and added categories,people,favourites and settings activities.

---
+++ app/src/main/java/com/justinmutsito/coolquotes/coolquotes/Favourites/FavouritesActivity.java
@@ -0,0 +1,15 @@
+package com.justinmutsito.coolquotes.coolquotes.Favourites;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import com.justinmutsito.coolquotes.coolquotes.R;
+
+public class FavouritesActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_favourites);
+    }
+}
--- app/src/main/java/com/justinmutsito/coolquotes/coolquotes/WelcomeActivity.java
+++ app/src/main/java/com/justinmutsito/coolquotes/coolquotes/WelcomeActivity.java
@@ -1,17 +1,41 @@
 package com.justinmutsito.coolquotes.coolquotes;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.DialogInterface;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.app.AlertDialog;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
 
+import com.justinmutsito.coolquotes.coolquotes.Categories.CategoriesActivity;
+import com.justinmutsito.coolquotes.coolquotes.Favourites.FavouritesActivity;
+import com.justinmutsito.coolquotes.coolquotes.People.PeopleActivity;
+import com.justinmutsito.coolquotes.coolquotes.Settings.SettingsActivity;
+
+import java.util.Random;
+
 import butterknife.Bind;
 import butterknife.ButterKnife;
+import butterknife.OnClick;
 
 public class WelcomeActivity extends AppCompatActivity {
 
+
+    private String[] mQuotes;
+    private int mPersonNumber;
+    private int mPosition;
+    private String mCurrentQuote;
+
     @Bind(R.id.faceImageView) ImageView mFace;
-    @Bind(R.id.quoteTextView) TextView mQuote;
+    @Bind(R.id.quoteLabel) TextView mQuote;
+    @Bind(R.id.categoriesButton) Button mCategories;
+    @Bind(R.id.peopleButton) Button mPeople;
+    @Bind(R.id.shareIcon) ImageView mShare;
+    @Bind(R.id.favouritesLabel) TextView mFavorites;
+    @Bind(R.id.settingsIcon) ImageView mSettings;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -19,7 +43,242 @@ public class WelcomeActivity extends AppCompatActivity {
         getSupportActionBar().hide();
         ButterKnife.bind(this);
 
-
+        checkTheme();
+        randomQuote();
+        getQuotes(mPersonNumber);
+        mCurrentQuote = mQuotes[mPosition];
+        mQuote.setText(mCurrentQuote);
 
     }
+
+    @OnClick(R.id.faceImageView)
+    public void animateFace() {
+        //Animate face
+    }
+
+
+    @OnClick(R.id.quoteLabel)
+    public void getQuote() {
+        //Share or add quote to favourites
+
+        AlertDialog.Builder builder = new AlertDialog.Builder(WelcomeActivity.this);
+
+        builder.setItems(R.array.quotesOptions, new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int position) {
+                if (position == 0) {
+                    //Share
+                    share(mCurrentQuote);
+                } else {
+                    //Add to favourites
+                }
+            }
+        });
+
+
+        AlertDialog dialog = builder.create();
+        dialog.show();
+    }
+
+
+    @OnClick(R.id.categoriesButton)
+    public void startActivityCategories() {
+        startActivity(new Intent(WelcomeActivity.this, CategoriesActivity.class));
+
+    }
+
+
+    @OnClick(R.id.peopleButton)
+    public void startActivityPeople() {
+        startActivity(new Intent(WelcomeActivity.this, PeopleActivity.class));
+    }
+
+    @OnClick(R.id.shareIcon)
+    public void shareAppDetails() {
+        //Share app with friends
+
+        String url = "https://play.google.com/store/apps/details?id=com.justinmutsito.standoutqoutes";
+        String message = "Check out this cool app " + url;
+        share(message);
+
+    }
+
+    @OnClick(R.id.favouritesLabel)
+    public void startActivityFavourites() {
+        startActivity(new Intent(WelcomeActivity.this, FavouritesActivity.class));
+    }
+
+    @OnClick(R.id.settingsIcon)
+    public void configSettings() {
+        startActivity(new Intent(WelcomeActivity.this, SettingsActivity.class));
+    }
+
+    private void randomQuote() {
+        //Sets randomly the person and quote position.
+
+        int NUMBER_OF_PEOPLE = 23; //Number of people in the people string array
+        int MINIMUM_NUMBER_OF_QUOTES = 10; //Minimum number of quotes provided by one person
+
+        Random r = new Random();
+        mPersonNumber = r.nextInt(NUMBER_OF_PEOPLE - 1);
+        mPosition = r.nextInt(MINIMUM_NUMBER_OF_QUOTES - 1);
+
+    }
+
+    private void getQuotes(int n) {
+
+        //Select which string array of quotes to use
+        switch (n) {
+
+            case 0: {
+                mQuotes = getResources().getStringArray(R.array.Albert_Einstein);
+                mFace.setImageResource(R.drawable.ic_account_circle_white_48dp);//Put person's face
+                break;
+            }
+            case 1: {
+                mQuotes = getResources().getStringArray(R.array.Abraham_Lincoln);
+                mFace.setImageResource(R.drawable.ic_account_circle_white_48dp);//Put person's face
+                break;
+            }
+
+
+            case 2: {
+                mQuotes = getResources().getStringArray(R.array.Benjamin_Franklin);
+                mFace.setImageResource(R.drawable.ic_account_circle_white_48dp);//Put person's face
+                break;
+            }
+
+            case 3: {
+                mQuotes = getResources().getStringArray(R.array.Bill_Gates);
+                mFace.setImageResource(R.drawable.ic_account_circle_white_48dp);//Put person's face
+                break;
+            }
+
+            case 4: {
+                mQuotes = getResources().getStringArray(R.array.Bill_Cosby);
+                mFace.setImageResource(R.drawable.ic_account_circle_white_48dp);//Put person's face
+                break;
+            }
+
+            case 5: {
+                mQuotes = getResources().getStringArray(R.array.Confucius);
+                mFace.setImageResource(R.drawable.ic_account_circle_white_48dp);//Put person's face
+                break;
+            }
+            case 6: {
+                mQuotes = getResources().getStringArray(R.array.Charles_Darwin);
+                mFace.setImageResource(R.drawable.ic_account_circle_white_48dp);//Put person's face
+                break;
+            }
+
+            case 7: {
+                mQuotes = getResources().getStringArray(R.array.Charles_Dickens);
+                mFace.setImageResource(R.drawable.ic_account_circle_white_48dp);//Put person's face
+                break;
+            }
+
+            case 8: {
+                mQuotes = getResources().getStringArray(R.array.Charlie_Chaplin);
+                mFace.setImageResource(R.drawable.ic_account_circle_white_48dp);//Put person's face
+                break;
+            }
+            case 9: {
+                mQuotes = getResources().getStringArray(R.array.Ernest_Hemingway);
+                mFace.setImageResource(R.drawable.ic_account_circle_white_48dp);//Put person's face
+                break;
+            }
+
+            case 10: {
+                mQuotes = getResources().getStringArray(R.array.Ernesto_Guevara);
+                mFace.setImageResource(R.drawable.ic_account_circle_white_48dp);//Put person's face
+                break;
+            }
+
+            case 11: {
+                mQuotes = getResources().getStringArray(R.array.George_Bernard_Shaw);
+                mFace.setImageResource(R.drawable.ic_account_circle_white_48dp);//Put person's face
+                break;
+            }
+            case 12: {
+                mQuotes = getResources().getStringArray(R.array.Henry_Ford);
+                mFace.setImageResource(R.drawable.ic_account_circle_white_48dp);//Put person's face
+                break;
+            }
+
+            case 13: {
+                mQuotes = getResources().getStringArray(R.array.Julian_Assange);
+                mFace.setImageResource(R.drawable.ic_account_circle_white_48dp);//Put person's face
+                break;
+            }
+            case 14: {
+                mQuotes = getResources().getStringArray(R.array.Karl_Marx);
+                mFace.setImageResource(R.drawable.ic_account_circle_white_48dp);//Put person's face
+                break;
+            }
+
+            case 15: {
+                mQuotes = getResources().getStringArray(R.array.Mahatma_Gandhi);
+                mFace.setImageResource(R.drawable.ic_account_circle_white_48dp);//Put person's face
+                break;
+            }
+            case 16: {
+                mQuotes = getResources().getStringArray(R.array.Mother_Teresa);
+                mFace.setImageResource(R.drawable.ic_account_circle_white_48dp);//Put person's face
+                break;
+            }
+
+            case 17: {
+                mQuotes = getResources().getStringArray(R.array.Mark_Twain);
+                mFace.setImageResource(R.drawable.ic_account_circle_white_48dp);//Put person's face
+                break;
+            }
+
+            case 18: {
+                mQuotes = getResources().getStringArray(R.array.Oscar_Wilde);
+                mFace.setImageResource(R.drawable.ic_account_circle_white_48dp);//Put person's face
+                break;
+            }
+
+            case 19: {
+                mQuotes = getResources().getStringArray(R.array.Socrates);
+                mFace.setImageResource(R.drawable.ic_account_circle_white_48dp);//Put person's face
+                break;
+            }
+            case 20: {
+                mQuotes = getResources().getStringArray(R.array.Steven_Jobs);
+                mFace.setImageResource(R.drawable.ic_account_circle_white_48dp);//Put person's face
+                break;
+            }
+
+            case 21: {
+                mQuotes = getResources().getStringArray(R.array.William_Shakespeare);
+                mFace.setImageResource(R.drawable.ic_account_circle_white_48dp);//Put person's face
+                break;
+            }
+
+
+            default: {
+                mQuotes = getResources().getStringArray(R.array.Warren_Buffett);
+                mFace.setImageResource(R.drawable.ic_account_circle_white_48dp);//Put person's face
+
+            }
+        }
+
+    }
+
+    private void share(String text) {
+        //Share text
+        Intent shareIntent = new Intent(Intent.ACTION_SEND);
+        shareIntent.setType("text/plain");
+        shareIntent.putExtra(Intent.EXTRA_TEXT, text);
+        startActivity(Intent.createChooser(shareIntent, getString(R.string.share)));
+    }
+
+
+    private void checkTheme() {
+        //Check the prefered theme
+    }
+
 }
+
+
