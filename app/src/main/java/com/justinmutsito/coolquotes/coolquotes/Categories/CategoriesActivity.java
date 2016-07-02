83245190d5a1dddbfc2bd2ebdb6adc7ea65c297b
Justin Mutsito <justinmutsito@gmail.com>
2016-07-02 22:12:15 +0100

    Implemented categories activity and added category activity.

--- app/src/main/java/com/justinmutsito/coolquotes/coolquotes/Categories/CategoriesActivity.java
+++ app/src/main/java/com/justinmutsito/coolquotes/coolquotes/Categories/CategoriesActivity.java
@@ -1,15 +1,27 @@
 package com.justinmutsito.coolquotes.coolquotes.Categories;
 
+import android.app.ListActivity;
 import android.os.Bundle;
-import android.support.v7.app.AppCompatActivity;
+import android.widget.ArrayAdapter;
 
 import com.justinmutsito.coolquotes.coolquotes.R;
 
-public class CategoriesActivity extends AppCompatActivity {
+public class CategoriesActivity extends ListActivity {
+    private String[] mCategories;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_categories);
+
+
+        mCategories = getResources().getStringArray(R.array.categories);
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this,android.R.layout.simple_list_item_1,mCategories);
+        setListAdapter(adapter);
     }
+
+
+
+
+
 }
--- app/src/main/java/com/justinmutsito/coolquotes/coolquotes/Categories/CategoryActivity.java
+++ app/src/main/java/com/justinmutsito/coolquotes/coolquotes/Categories/CategoryActivity.java
@@ -1,20 +1,17 @@
-package com.justinmutsito.coolquotes.coolquotes.Favourites;
+package com.justinmutsito.coolquotes.coolquotes.Categories;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 
 import com.justinmutsito.coolquotes.coolquotes.R;
 
-public class FavouritesActivity extends AppCompatActivity {
-
-
+public class CategoryActivity extends AppCompatActivity {
+    private int mPosition;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_favourites);
-
-
-
+        setContentView(R.layout.activity_category);
+        getSupportActionBar().hide();
 
 
 
