f189c484bbd07777574cb85f852494a79e694c78
Justin Mutsito <justinmutsito@gmail.com>
2016-07-15 07:18:09 +0100

    Finished AuthorsActivity

--- app/src/main/java/com/justinmutsito/coolquotes/coolquotes/Authors/AuthorsActivity.java
+++ app/src/main/java/com/justinmutsito/coolquotes/coolquotes/Authors/AuthorsActivity.java
@@ -1,22 +1,28 @@
 package com.justinmutsito.coolquotes.coolquotes.Authors;
 
+import android.app.ListActivity;
 import android.os.Bundle;
-import android.support.v7.app.AppCompatActivity;
 
 import com.justinmutsito.coolquotes.coolquotes.R;
 
-import butterknife.ButterKnife;
+public class AuthorsActivity extends ListActivity {
 
-public class AuthorsActivity extends AppCompatActivity {
+    public int[] AuthorFaces = {R.drawable.ic_account_circle_grey600_48dp, R.drawable.ic_account_circle_grey600_48dp, R.drawable.ic_account_circle_grey600_48dp, R.drawable.ic_account_circle_grey600_48dp, R.drawable.ic_account_circle_grey600_48dp, R.drawable.ic_account_circle_grey600_48dp
+            , R.drawable.ic_account_circle_grey600_48dp, R.drawable.ic_account_circle_grey600_48dp, R.drawable.ic_account_circle_grey600_48dp, R.drawable.ic_account_circle_grey600_48dp, R.drawable.ic_account_circle_grey600_48dp, R.drawable.ic_account_circle_grey600_48dp
+            , R.drawable.ic_account_circle_grey600_48dp, R.drawable.ic_account_circle_grey600_48dp, R.drawable.ic_account_circle_grey600_48dp, R.drawable.ic_account_circle_grey600_48dp, R.drawable.ic_account_circle_grey600_48dp, R.drawable.ic_account_circle_grey600_48dp,
+            R.drawable.ic_account_circle_grey600_48dp, R.drawable.ic_account_circle_grey600_48dp, R.drawable.ic_account_circle_grey600_48dp};
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_authors);
-        ButterKnife.bind(this);
+        String[] Authors = getResources().getStringArray(R.array.authors);
 
-
-
+        AuthorsAdapter adapter = new AuthorsAdapter(this, Authors, AuthorFaces);
+        setListAdapter(adapter);
 
     }
+
+
 }
---
+++ app/src/main/java/com/justinmutsito/coolquotes/coolquotes/Authors/AuthorsAdapter.java
@@ -0,0 +1,80 @@
+package com.justinmutsito.coolquotes.coolquotes.Authors;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.justinmutsito.coolquotes.coolquotes.R;
+
+/**
+ * Created by justin on 7/14/16.
+ */
+public class AuthorsAdapter extends BaseAdapter {
+
+    private Context mContext;
+    private String[] mAuthors;
+    private int[] mAuthorFace;
+
+
+    public AuthorsAdapter(AuthorsActivity authorActivity, String[] Authors, int[] AuthorFace) {
+        mAuthors = Authors;
+        mContext = authorActivity;
+        mAuthorFace = AuthorFace;
+
+    }
+
+    @Override
+    public int getCount() {
+        // TODO Auto-generated method stub
+        return mAuthors.length;
+    }
+
+    @Override
+    public Object getItem(int position) {
+        // TODO Auto-generated method stub
+        return position;
+    }
+
+    @Override
+    public long getItemId(int position) {
+        // TODO Auto-generated method stub
+        return position;
+    }
+
+    public class ViewHolder {
+        public TextView mName;
+        public ImageView mIconFace;
+    }
+
+    @Override
+    public View getView(final int position, View convertView, ViewGroup parent) {
+        // TODO Auto-generated method stub
+        ViewHolder holder;
+        if (convertView == null) {
+            convertView = LayoutInflater.from(mContext).inflate(R.layout.author_layout, null);
+            holder = new ViewHolder();
+            holder.mName = (TextView) convertView.findViewById(R.id.nameLabel);
+            holder.mIconFace = (ImageView) convertView.findViewById(R.id.faceImageView);
+            holder.mName.setText(mAuthors[position]);
+            holder.mIconFace.setImageResource(mAuthorFace[position]);
+            convertView.setTag(holder);
+        } else {
+            holder = (ViewHolder) convertView.getTag();
+        }
+
+        convertView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(mContext, "working", Toast.LENGTH_SHORT).show();
+            }
+        });
+        return convertView;
+
+
+    }
+}
